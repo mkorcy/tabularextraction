20d580971882a3362f2b687396582f683f5aa372
Mike Korcynski <mkorcy@gmail.com>
2019-06-02 12:30:15 -0400

    just a utility script for cleaning up after abended runs, not really needed.

---
+++ structure/crop/clean_dirs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# utility script for cleaning up all the working
+# directories except for input_pdfs
+rm -rf ./first_crop_images
+rm -rf ./output_images
+rm -rf ./second_crop_images
+rm -rf ./output_pdfs
+rm -rf ./deskewed_images
+rm -rf ./erode_images
